9bffac5659e0bcef1307176a966e188f408ff60c
nilmarlanna <nilmar.lanna@outlook.com.br>
2023-05-01 20:51:59 -0300

    ultimo commit

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,10 +1,30 @@
-import Cabecalho from "../../componentes/Cabecalho";
+import banner from "./banner.png";
+import styles from "./PaginaInicial.module.scss";
 
-export default function PaginaInicial(){
+import Cabecalho from "componentes/Cabecalho";
+import Menu from "componentes/Menu";
+import Rodape from "componentes/Rodape";
+import Galeria from "componentes/Galeria";
+import Populares from "componentes/Populares";
+
+export default function PaginaInicial() {
   return (
     <>
-    <Cabecalho />
-    <Menu />
+      <Cabecalho />
+      <main>
+        <section className={styles.principal}>
+          <Menu />
+          <div className={styles.principal__imagem}>
+            <h1>A galeria mais completa do espaço</h1>
+            <img src={banner} alt="A imagem da terra vista do espaço" />
+          </div>
+        </section>
+        <div className={styles.galeria}>
+          <Galeria />
+          <Populares />
+        </div>
+      </main>
+      <Rodape />
     </>
-  )
+  );
 }
